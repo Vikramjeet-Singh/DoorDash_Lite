29e7358c7fc7a7027d15675175ea0cf830bc0477
Unknown <saini.vikram17@gmail.com>
2018-04-07 01:20:49 -0700

    Added Reachability and updated project settings

---
+++ DoorDashLite/DoorDashLite/SupportingFiles/DoorDashLite-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  DoorDashLite-Bridging-Header.h
+//  DoorDashLite
+//
+//  Created by Vikramjeet Singh on 4/7/18.
+//  Copyright © 2018 Vikramjeet Singh. All rights reserved.
+//
+
+#ifndef DoorDashLite_Bridging_Header_h
+#define DoorDashLite_Bridging_Header_h
+
+#import "Reachability.h"
+
+#endif /* DoorDashLite_Bridging_Header_h */
+
