1223da4df3353d7977b3d378419af356503ee881
Unknown <saini.vikram17@gmail.com>
2018-04-07 04:48:25 -0700

    Added NearbyRestaurant TableViewCell file

---
+++ DoorDashLite/DoorDashLite/View/NearbyRestaurantCell.swift
@@ -0,0 +1,24 @@
+//
+//  NearbyRestaurantCell.swift
+//  DoorDashLite
+//
+//  Created by Vikramjeet Singh on 4/7/18.
+//  Copyright © 2018 Vikramjeet Singh. All rights reserved.
+//
+
+import UIKit
+
+class NearbyRestaurantCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
